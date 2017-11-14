453c34f0ede8989fde3031f2c6d5b88b10e4e5a2
Izabela Borges <belacb.dev@gmail.com>
2017-11-14 11:30:59 -0200

    Add file from Udacity Intro to Data Science course

---
+++ courses/udacity/intro-to-data-science/lesson4/quiz5.py
@@ -0,0 +1,47 @@
+import csv
+
+def fix_turnstile_data(filenames):
+    '''
+    Filenames is a list of MTA Subway turnstile text files. A link to an example
+    MTA Subway turnstile text file can be seen at the URL below:
+    http://web.mta.info/developers/data/nyct/turnstile/turnstile_110507.txt
+    
+    As you can see, there are numerous data points included in each row of the
+    a MTA Subway turnstile text file. 
+
+    You want to write a function that will update each row in the text
+    file so there is only one entry per row. A few examples below:
+    A002,R051,02-00-00,05-28-11,00:00:00,REGULAR,003178521,001100739
+    A002,R051,02-00-00,05-28-11,04:00:00,REGULAR,003178541,001100746
+    A002,R051,02-00-00,05-28-11,08:00:00,REGULAR,003178559,001100775
+    
+    Write the updates to a different text file in the format of "updated_" + filename.
+    For example:
+        1) if you read in a text file called "turnstile_110521.txt"
+        2) you should write the updated data to "updated_turnstile_110521.txt"
+
+    The order of the fields should be preserved. Remember to read through the 
+    Instructor Notes below for more details on the task. 
+    
+    In addition, here is a CSV reader/writer introductory tutorial:
+    http://goo.gl/HBbvyy
+    
+    You can see a sample of the turnstile text file that's passed into this function
+    and the the corresponding updated file by downloading these files from the resources:
+    
+    Sample input file: turnstile_110528.txt
+    Sample updated file: solution_turnstile_110528.txt
+    '''
+    for name in filenames:
+        with open(name, 'rb') as f:
+            reader = csv.reader(f)
+            all_rows = list()
+            for row in reader:
+                minus_first3 = row[3:]
+                for i, j, k, l, m in zip(minus_first3[::5],minus_first3[1::5],minus_first3[2::5],minus_first3[3::5],minus_first3[4::5]):
+                    new_row = row[:3] + [i, j, k, l, m]
+                    all_rows.append(new_row)
+            updated = 'updated_' + name
+            with open(updated, 'wb') as ff:
+                writer = csv.writer(ff)
+                writer.writerows(all_rows)               
