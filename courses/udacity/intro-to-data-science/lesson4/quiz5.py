9ac60a7a14a6bf85e23fd0f66ce682e9ca714b49
Sourcery AI <>
2023-08-26 01:33:54 +0000

    'Refactored by Sourcery'

--- courses/udacity/intro-to-data-science/lesson4/quiz5.py
+++ courses/udacity/intro-to-data-science/lesson4/quiz5.py
@@ -4,13 +4,20 @@ def fix_turnstile_data(filenames):
     for name in filenames:
         with open(name, 'rb') as f:
             reader = csv.reader(f)
-            all_rows = list()
+            all_rows = []
             for row in reader:
                 minus_first3 = row[3:]
-                for i, j, k, l, m in zip(minus_first3[::5],minus_first3[1::5],minus_first3[2::5],minus_first3[3::5],minus_first3[4::5]):
-                    new_row = row[:3] + [i, j, k, l, m]
-                    all_rows.append(new_row)
-            updated = 'updated_' + name
+                all_rows.extend(
+                    row[:3] + [i, j, k, l, m]
+                    for i, j, k, l, m in zip(
+                        minus_first3[::5],
+                        minus_first3[1::5],
+                        minus_first3[2::5],
+                        minus_first3[3::5],
+                        minus_first3[4::5],
+                    )
+                )
+            updated = f'updated_{name}'
             with open(updated, 'wb') as ff:
                 writer = csv.writer(ff)
                 writer.writerows(all_rows)               
